529cc78181d9cd74a5398ea86c0aa39461484821
Brock Jameson <bjameson@talentpath.com>
2019-02-19 11:33:13 -0800

    closed #1

--- js/toggleActive.js
+++ js/toggleActive.js
@@ -3,10 +3,8 @@
  * @param {Event} event
  */
 export default function toggleActive(event) {
-  //   const toggleButton = document.querySelectorAll(".save-button");
-  //   toggleButton.forEach(button => ".save-button");
   // get the parent element
-  console.log(event);
+  //   console.log(event);
   const editableText = event.target.parentElement;
   // toggle the class active
   editableText.classList.toggle("active");
